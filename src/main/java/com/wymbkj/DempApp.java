5d3caad797024814713b72785e346dfa8c4fbbfa
chenjialu <912168191@qq.com>
2022-08-15 12:19:17 +0800

    1

---
+++ src/main/java/com/wymbkj/DempApp.java
@@ -0,0 +1,17 @@
+package com.wymbkj;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DempApp {
+
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(DempApp.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
